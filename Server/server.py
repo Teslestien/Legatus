10c11a3e607257158e85e5a25bd52169701c6989
Teslestien <teslestien@gmail.com>
2023-04-06 16:58:09 +0530

    Initial commit

---
+++ Server/server.py
@@ -0,0 +1,56 @@
+from flask import Flask, request
+from replit import db
+from datetime import datetime
+import json
+
+app = Flask("Legatus", template_folder='templates', static_folder='static')
+
+
+@app.route('/', methods=['GET'])
+def start():
+  return "hello"
+
+
+@app.route('/receive', methods=['GET'])
+def receive():
+  receiver = request.args.get("user")
+  to_receive = []
+  messages = json.loads(open("messages.json").read())
+  try:
+    if receiver == "Questo":
+      for i in range(len(messages)):
+        if messages[i]["Sender"] == "Exclay":
+          messages[i]["Content"] = messages[i]["Content"].replace(" ", "+")
+          to_receive.append(messages[i])
+          del messages[i]
+    else:
+      for i in range(len(messages)):
+        if messages[i]["Sender"] == "Questo":
+          messages[i]["Content"] = messages[i]["Content"].replace(" ", "+")
+          to_receive.append(messages[i])
+          del messages[i]
+  except:
+    return "[]\n"
+
+  with open("messages.json", "w") as outfile:
+    outfile.write(json.dumps(messages))
+  messages = []
+  return to_receive
+
+
+@app.route('/send', methods=['GET'])
+def send():
+  messages = json.loads(open("messages.json").read())
+  sender = request.args.get("user")
+  message = request.args.get("message")
+  time = datetime.now().strftime("%H:%M:%S:%f")
+  msg = {"Sender": sender, "Time": time, "Content": message}
+  print(msg)
+  messages.append(msg)
+  with open("messages.json", "w") as outfile:
+    outfile.write(json.dumps(messages))
+  print(sender, message, time, sep="\n")
+  return "[]\n"
+
+
+app.run(host="0.0.0.0", port=890)
